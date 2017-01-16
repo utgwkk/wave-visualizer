9ab21258ae2ef44d353c0801267ec1386a8d59da
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-01-16 13:36:18 +0900

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp')
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
+
+gulp.task('default', () => {
+  return browserify({
+    entries: './src/js/index.js'
+  })
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./static/js'))
+})
